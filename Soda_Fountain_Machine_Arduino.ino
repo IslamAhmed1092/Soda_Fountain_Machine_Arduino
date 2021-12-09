81efe5941b6b7b648b8f388abb2a4bc237fc0b27
IslamAhmed1092 <eslam1092@hotmail.com>
2021-12-09 13:09:48 +0200

    handle water levels and no drink available

--- Soda_Fountain_Machine_Arduino.ino
+++ Soda_Fountain_Machine_Arduino.ino
@@ -1,7 +1,9 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
-float RULERHEIGHT =21;
-bool debugCode=true;
+float RULERHEIGHT = 21;
+float CUP1 = 2;
+float CUP2 = 3;
+bool debugCode = false;
 enum operationState
 {
   OFF,
@@ -20,6 +22,7 @@ class Motor
 private:
   int pin;
   operationState state;
+
 public:
   Motor(int pin);
   void turnOn()
@@ -35,7 +38,6 @@ public:
   ~Motor();
 };
 
-
 Motor::Motor(int pin)
 {
   this->state = operationState::OFF;
@@ -45,7 +47,6 @@ Motor::Motor(int pin)
 Motor::~Motor()
 {
 }
-
 
 class Ultrasonic
 {
@@ -80,8 +81,6 @@ float Ultrasonic::getDistance()
   return distance;
 }
 
-
-
 float Ultrasonic::getHeight()
 {
 
@@ -90,7 +89,7 @@ float Ultrasonic::getHeight()
   digitalWrite(trig, LOW);
   float t = pulseIn(echo, HIGH, 28500);
   float distance = t / 57;
-  return RULERHEIGHT-distance;
+  return RULERHEIGHT - distance;
 }
 
 // Global Variables section
@@ -100,10 +99,11 @@ const int trig = 12, echo = 11;
 const int buzzer = 13;
 const int motor_pin = 10;
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-
-// 
+const int red_led = 0;
+const int blue_led = 1;
+//
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-Ultrasonic ultrasonic(trig,echo);
+Ultrasonic ultrasonic(trig, echo);
 const int ROW_NUM = 4;
 const int COLUMN_NUM = 3;
 
@@ -119,7 +119,6 @@ byte pin_column[COLUMN_NUM] = {A3, A4, A5};
 Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
 Motor motor = Motor(motor_pin);
 
-
 ProgressState current_state = INPUT_PASSWORD;
 const String real_password = "1234";
 String input_password = "";
@@ -127,10 +126,10 @@ String input_password = "";
 bool first_time = true;
 uint8_t cup_size;
 
-
 void setup()
 {
-  if(debugCode){
+  if (debugCode)
+  {
 
     Serial.begin(9600);
   }
@@ -138,6 +137,8 @@ void setup()
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
   pinMode(motor_pin, OUTPUT);
+  pinMode(red_led, OUTPUT);
+  pinMode(blue_led, OUTPUT);
   digitalWrite(motor_pin, HIGH);
   lcd.begin(16, 2);
 }
@@ -163,7 +164,8 @@ void alert()
 }
 void handlePassword()
 {
-  if(first_time) {  
+  if (first_time)
+  {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Password:");
@@ -209,7 +211,8 @@ void handlePassword()
 
 void handleMode()
 {
-  if(first_time) {  
+  if (first_time)
+  {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("1:Medium 2:Large");
@@ -240,11 +243,11 @@ void handleMode()
   }
 }
 
-
 long current_time = 0, motor_time = 0;
 float first_height;
 void pourCocaCola()
 {
+
   // pour the coca
   // check for complete of the coca
   // store in the EEPROM
@@ -255,57 +258,105 @@ void pourCocaCola()
   
   */
   // in finished input password
-  if(first_time) {  
+  if (first_time)
+  {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Pouring drink ");
     lcd.setCursor(0, 1);
-    
-    if(cup_size == 1) 
+
+    if (cup_size == 1)
       lcd.print("Medium");
     else
       lcd.print("Large");
-    
+
     first_time = false;
-    
+
     motor.turnOn();
     motor_time = millis();
     first_height = ultrasonic.getHeight();
   }
-  
+
   current_time = millis();
   lcd.setCursor(7, 1);
   lcd.print(((current_time - motor_time) / 1000) + 1);
-  if((cup_size == 1 && current_time - motor_time >= 5000) || (cup_size == 2 && current_time - motor_time >= 10000)) {
+
+  float heightDiff = first_height - ultrasonic.getHeight();
+  lcd.setCursor(9, 1);
+  lcd.print (heightDiff);
+  if ((cup_size == 1 && heightDiff > CUP1 && current_time - motor_time >= 5000) || (cup_size == 2 && heightDiff > CUP2 && current_time - motor_time >= 10000))
+  {
     motor.turnOff();
     current_state = ProgressState::INPUT_PASSWORD;
     first_time = true;
   }
- 
 }
+
+long oldMillis = 0, currentMillis;
+float current_height;
+bool is_empty = false;
 void loop()
 {
- if(debugCode){
+  if (debugCode)
+  {
     float distance = ultrasonic.getDistance();
 
     Serial.print("sonic distance is ");
     Serial.println(distance);
-
   }
-  switch (current_state)
+
+  // lcd.clear();
+  // lcd.setCursor(0, 0);
+  // lcd.print("height is ");
+  // lcd.setCursor(0, 1);
+  // lcd.print(current_height);
+  // delay(1000);
+  // check if there is space
+  currentMillis = millis();
+  if (currentMillis - oldMillis > 100)
   {
-  case ProgressState::INPUT_PASSWORD:
-    handlePassword();
-    break;
+    current_height = ultrasonic.getHeight();
+    if (current_height < 3.5)
+    {
+      digitalWrite(red_led, HIGH);
+      digitalWrite(blue_led, LOW);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("no coca available");
 
-  case ProgressState::CHOOSE_MODE:
-    handleMode();
-    break;
+      input_password = "";
+      current_state = ProgressState::INPUT_PASSWORD;
+      motor.turnOff();
 
-  case ProgressState::POUR_MODE:
-    pourCocaCola();
-    break;
-  default:
-    break;
+      is_empty = true;
+    }
+    else if (current_height > 4)
+    {
+      digitalWrite(red_led, LOW);
+      digitalWrite(blue_led, HIGH);
+      if (is_empty) {
+      is_empty = false;
+      first_time = true;
+      }
+    }
+    oldMillis = currentMillis;
   }
+
+  if (!is_empty)
+    switch (current_state)
+    {
+    case ProgressState::INPUT_PASSWORD:
+      handlePassword();
+      break;
+
+    case ProgressState::CHOOSE_MODE:
+      handleMode();
+      break;
+
+    case ProgressState::POUR_MODE:
+      pourCocaCola();
+      break;
+    default:
+      break;
+    }
 }
